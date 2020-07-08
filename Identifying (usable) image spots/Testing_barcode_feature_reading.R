53d31a2598c8b8a48ab5e0fb4a5cb679a7a2279c
Cole <diiannic@carleton.edu>
2020-07-08 14:04:26 -0400

    added testing barcode reading file

--- Identifying (usable) image spots/Testing_barcode_feature_reading.R
+++ Identifying (usable) image spots/Testing_barcode_feature_reading.R
@@ -1,25 +1,15 @@
-barcodes <- read.table(file = "/Users/colten/Desktop/Perkins_Lab_ST/Identifying (usable) image spots/data/filtered_feature_bc_matrix/barcodes.tsv", sep = '\t')
+library(Seurat)
+feature_matrix <- Load10X_Spatial(data.dir = "/Users/colten/Desktop/Perkins_Lab_ST/Identifying (usable) image spots/data",
+                         filename = "V1_Breast_Cancer_Block_A_Section_1_filtered_feature_bc_matrix.h5",
+                         assay = "Spatial",
+                         slice = "slice1",
+                         filter.matrix = TRUE,
+                         to.upper = FALSE)
 
-features <- read.table(file = "/Users/colten/Desktop/Perkins_Lab_ST/Identifying (usable) image spots/data/filtered_feature_bc_matrix/features.tsv", sep = '\t')
-
-library(Matrix)
-#This should be read into a more easily understandable table
-bf_matrix <- Matrix::readMM("/Users/colten/Desktop/Perkins_Lab_ST/Identifying (usable) image spots/data/filtered_feature_bc_matrix/matrix.mtx")
-
-colnames(bf_matrix) = barcodes$V1
-rownames(bf_matrix) = features$V1
-
-
-# go through the bf_matrix and filter for each spot: (set barcode to rowname by finding barcode in barcodes)
-# within the spot go through each gene and lookup the gene ID index from features.
-# assign the gene column at the spot row to the value given
+bf_matrix <- as.matrix(GetAssayData(object = feature_matrix, slot = "counts"))
 
 
 
-library(readtext)
-text <- read.t
 
-dat <- read.table(text="person1    12    15
-    person2    15    18
-    person3    20    14", stringsAsFactors=FALSE
-)
+
+
