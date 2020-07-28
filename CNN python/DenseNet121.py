83a448ef08335850e809d96a0b994816c1dcf9a7
Cole <diiannic@carleton.edu>
2020-07-28 16:11:07 -0400

    updated densenet python file to take data from multiple folders

--- CNN python/DenseNet121.py
+++ CNN python/DenseNet121.py
@@ -58,14 +58,14 @@ def load_bfm(dir, count_file, spot_locations, number_of_genes_to_predict):
     sc.pp.filter_cells(adata, min_counts=5000)
     sc.pp.filter_cells(adata, max_counts=35000)
 
-    #normalize the data with scRNA normalization
+    # normalize the data with scRNA normalization
     # useful site: https://anndata.readthedocs.io/en/stable/anndata.AnnData.html#anndata.AnnData
     sc.pp.normalize_total(adata, inplace=True)
     sc.pp.log1p(adata)
     sc.pp.highly_variable_genes(adata, flavor="seurat", n_top_genes = number_of_genes_to_predict)
 
     mean_expression = adata.var["means"]
-    mean_expression.sort_values(ascending = False)
+    mean_expression.sort_values(ascending=False)
     adata = adata[:, mean_expression.index[:number_of_genes_to_predict]]
 
     #equivalent to the GetAssayData
@@ -77,7 +77,7 @@ def load_bfm(dir, count_file, spot_locations, number_of_genes_to_predict):
     sorted_barcodes = barcodes
     sorted_barcodes.sort()
 
-    if barcodes == sorted_barcodes:
+    if barcodes == sorted_barcodes: #todo make sure bfm and spot_locations really do match!
         spot_locations.index = spot_locations["barcode"]
         spot_locations.sort_index(inplace=True)
         for i in spot_locations.index:
@@ -91,19 +91,18 @@ def load_bfm(dir, count_file, spot_locations, number_of_genes_to_predict):
     return barcode_feature_matrix, spot_locations
 
 
-    # defining a function to get a cropped spot image given the x and y coordinates of the center of the spot
-def Crop_Spot(img_array, x, y, cropped_img_width):
+# defining a function to get a cropped spot image given the x and y coordinates of the center of the spot
+def crop_spot(img_array, x, y, cropped_img_width):
     cropped_img = img_array[int(y - (cropped_img_width / 2)):int(y + (cropped_img_width / 2)),
                   int(x - (cropped_img_width / 2)):int(x + (cropped_img_width / 2))]
 
     return cropped_img
 
 
-def generate_train_test_indexes(number_of_samples_to_use, number_of_spots):
+def generate_train_test_indexes(number_of_samples_to_use):
     # partition data indexes into train and test
-    train_test_indexes = [i for i in range(number_of_spots)]
+    train_test_indexes = [i for i in range(number_of_samples_to_use)]
     random.shuffle(train_test_indexes)
-    # train_test_indexes = train_test_indexes[:number_of_samples_to_use]
     train_indexes = train_test_indexes[:int(0.7 * len(train_test_indexes))]
     train_indexes.sort()
     test_indexes = train_test_indexes[int(0.7 * len(train_test_indexes)):]
@@ -123,21 +122,19 @@ def get_cropped_images(img_path, spot_locations, cropped_img_width, train_indexe
     # crop image into train and test
     training_cropped_img_list = []
     for i in train_indexes:
-        cropped_img = Crop_Spot(img_array, spot_locations["pxl_row_in_fullres_xValue"][i],
+        cropped_img = crop_spot(img_array, spot_locations["pxl_row_in_fullres_xValue"][i],
                                 spot_locations["pxl_col_in_fullres_yValue"][i],
                                 cropped_img_width)
         training_cropped_img_list.append(cropped_img)
     training_cropped_img_list = np.array(training_cropped_img_list)
-    # training_cropped_img_list = (training_cropped_img_list - [training_cropped_img_list[:, :, :, 0].mean(), training_cropped_img_list[:, :, :, 1].mean(), training_cropped_img_list[:, :, :, 2].mean()])
 
     testing_cropped_img_list = []
     for i in test_indexes:
-        cropped_img = Crop_Spot(img_array, spot_locations["pxl_row_in_fullres_xValue"][i],
+        cropped_img = crop_spot(img_array, spot_locations["pxl_row_in_fullres_xValue"][i],
                                 spot_locations["pxl_col_in_fullres_yValue"][i],
                                 cropped_img_width)
         testing_cropped_img_list.append(cropped_img)
     testing_cropped_img_list = np.array(testing_cropped_img_list)
-    # testing_cropped_img_list = (testing_cropped_img_list - [training_cropped_img_list[:, :, :, 0].mean(), training_cropped_img_list[:, :, :, 1].mean(), training_cropped_img_list[:, :, :, 2].mean()])
     return training_cropped_img_list, testing_cropped_img_list
 
 
@@ -149,16 +146,16 @@ def partition_bfm(bfm, train_indexes, test_indexes):
 
 
 def find_pove(bfm, predictions, number_of_samples, number_of_genes):
-    totalPOVE = []
+    total_pove = []
     for index in range(number_of_genes): #for every gene we predict
         actual_data_vairance = bfm[:number_of_samples, index].var() #TV
         residuals = bfm[:number_of_samples, index] - predictions[:number_of_samples, index]
         residuals_variance = residuals.var() #RV
         explained_variance = actual_data_vairance - residuals_variance #EV
         if actual_data_vairance != 0:
-           POVE = 100*(explained_variance) / actual_data_vairance
-           totalPOVE.append(POVE)
-    return sum(totalPOVE)/len(totalPOVE)
+            pove = 100*explained_variance / actual_data_vairance
+            total_pove.append(pove)
+    return sum(total_pove)/len(total_pove)
 
 
 def main():
@@ -168,32 +165,64 @@ def main():
     cropped_img_width = 224
     number_of_genes_to_predict = 100
     number_of_samples_to_use = 1000  # 2938
-    train_indexes, test_indexes = generate_train_test_indexes(number_of_samples_to_use, number_of_samples_to_use)
+    use_max_samples = True  # This will overwrite the number_of_samples_to_use if True
 
-    base_path = "/Users/colten/Desktop/Perkins_Lab_ST/Identifying (usable) image spots/data"
-    #USE CURRENT WORKING DIRECTORY AS THE FIRST PART OF BASE_PATH
+    # Setting cwd and base_path to each data set folder in the same directory as this file
     cwd = str(os.getcwd())
     file = list(filter(lambda x: "ST_" in x, os.listdir(cwd)))
     if len(file) >= 1:
-        print("Importing data from " + str(len(file)) + " datasets")
-    quit()
-    file = file[0]
+        print("Importing data from " + str(len(file)) + " data sets")
 
-    # setting file paths
-    spot_location_path = base_path + "/spatial/" + get_file_path_by_substring("tissue_positions_list.csv", base_path + "/spatial")
-    bfm_dir = base_path
-    bfm_filename = get_file_path_by_substring("filtered_feature_bc_matrix.h5", base_path)
-    image_path = base_path + "/" + get_file_path_by_substring("image.tif", base_path)
+    # initializing data bins
+    training_cropped_img_list = "empty"
+    testing_cropped_img_list = "empty"
+    training_bfm = "empty"
+    testing_bfm = "empty"
 
-    # loading data
-    spot_locations = get_spot_locations(spot_location_path)
-    bfm, spot_locations = load_bfm(bfm_dir, bfm_filename, spot_locations, number_of_genes_to_predict)
-    training_cropped_img_list, testing_cropped_img_list = get_cropped_images(image_path,
-                                                                             spot_locations,
-                                                                             cropped_img_width,
-                                                                             train_indexes,
-                                                                             test_indexes)
-    training_bfm, testing_bfm = partition_bfm(bfm, train_indexes, test_indexes)
+    # Training the CNN on all data sets except last one
+    for i in file:
+        base_path = cwd + "/" + i
+        print(base_path)
+
+        # setting file paths
+        spot_location_path = base_path + "/spatial/" + get_file_path_by_substring("tissue_positions_list.csv", base_path + "/spatial")
+        bfm_dir = base_path
+        bfm_filename = get_file_path_by_substring("filtered_feature_bc_matrix.h5", base_path)
+        image_path = base_path + "/" + get_file_path_by_substring("image.tif", base_path)
+
+        # loading data
+        spot_locations = get_spot_locations(spot_location_path)
+        bfm, spot_locations = load_bfm(bfm_dir, bfm_filename, spot_locations, number_of_genes_to_predict)
+        if use_max_samples:
+            number_of_samples_to_use = len(bfm)
+        train_indexes, test_indexes = generate_train_test_indexes(number_of_samples_to_use)
+
+        # Getting cropped image np arrays
+        training_cropped_img_list_i, testing_cropped_img_list_i = get_cropped_images(image_path,
+                                                                                    spot_locations,
+                                                                                    cropped_img_width,
+                                                                                    train_indexes,
+                                                                                    test_indexes)
+        print(training_cropped_img_list_i.shape)
+        if type(training_cropped_img_list) == str and type(testing_cropped_img_list) == str:
+            training_cropped_img_list, testing_cropped_img_list = training_cropped_img_list_i, testing_cropped_img_list_i
+        else:
+            training_cropped_img_list = np.concatenate((training_cropped_img_list, training_cropped_img_list_i))
+            testing_cropped_img_list = np.concatenate((testing_cropped_img_list, testing_cropped_img_list_i))
+
+        # Getting bfm np arrays
+        training_bfm_i, testing_bfm_i = partition_bfm(bfm,
+                                                    train_indexes,
+                                                    test_indexes)
+        print(training_bfm_i.shape)
+        if type(training_bfm) == str and type(testing_bfm) == str:
+            training_bfm, testing_bfm = training_bfm_i, testing_bfm_i
+        else:
+            training_bfm = np.concatenate((training_bfm, training_bfm_i))
+            testing_bfm = np.concatenate((testing_bfm, testing_bfm_i))
+
+    print(training_cropped_img_list.shape)
+    print(training_bfm.shape)
 
     # Neural Network
     print("made it to end")
